e7791b015129efcc428c86b6be1eb7f8e9073ff5
sina sima <simacna@gmail.com>
2019-08-14 14:17:28 -0400

    refactoring routes

--- Odin/projects/express-locallibrary/routes/users.js
+++ Odin/projects/express-locallibrary/routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+//right now the route is /users/cool, but if
+//change app.use('/', usersRouter); in app.js
+//to have /users, below i can get rid of the /users
+//and only have /cool
+router.get('/users/cool', (req, res) => {
+  res.send('hola hola');
 });
 
 module.exports = router;
