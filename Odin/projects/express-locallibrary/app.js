54bf95632fe038303c5e6273a96cfe7118f0744c
sina sima <simacna@gmail.com>
2019-08-13 21:29:16 -0400

    traversin app structure

--- Odin/projects/express-locallibrary/app.js
+++ Odin/projects/express-locallibrary/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+//express appliction object
 var app = express();
 
 // view engine setup
@@ -24,6 +26,8 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+
+  //why won't return(createError(404)); work?
   next(createError(404));
 });
 
